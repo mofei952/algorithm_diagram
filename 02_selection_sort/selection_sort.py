a4b36238ed3a7b3acc7a97a887b7cc6b3a10a613
mofei952 <695741200@qq.com>
2020-09-23 21:26:52 +0800

    add: selection sort

---
+++ 02_selection_sort/selection_sort.py
@@ -0,0 +1,15 @@
+def find_smallest_index(arr):
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < arr[smallest_index]:
+            smallest_index = i
+    return smallest_index
+
+def selection_sort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        index = find_smallest_index(arr)
+        new_arr.append(arr.pop(index))
+    return new_arr
+
+print(selection_sort([5, 3, 6, 2, 10]))
